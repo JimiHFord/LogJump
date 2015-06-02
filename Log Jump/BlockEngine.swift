12e10a97e24fd00a30850c8be9a991b766dd85aa
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-06-01 23:04:07 -0400

    fixed error

--- Log Jump/BlockEngine.swift
+++ Log Jump/BlockEngine.swift
@@ -12,7 +12,7 @@ class BlockEngine {
     let platforms = [ "short_block", "tall_block"]
     
     func pick() -> String {
-        let result = arc4random_uniform(UInt32(platforms.count))
-        return platforms[result == 0 ? 0 : 1]
+        let result = Int(arc4random_uniform(UInt32(platforms.count)))
+        return platforms[result]
     }
 }
